037e5d4e04f839de8d2843be0cad8cda080dec9d
nereus <nereus@sandro.lan>
2023-04-02 12:12:38 +0800

    add config and build

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+make download -j4 V=s
+make download -j4 V=s
+make -j4 V=s
---
+++ prebuild.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#git pull
+./scripts/feeds update -a
+./scripts/feeds update -a
+./scripts/feeds install -a
+./scripts/feeds install -a
